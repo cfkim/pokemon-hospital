fa330cc0818c2ee214e20351946b6a2283639259
Christine Kim <97631051+cfkim@users.noreply.github.com>
2023-11-02 18:27:51 -0400

    Create pokemon-db.php

---
+++ pokemon-db.php
@@ -0,0 +1,56 @@
+<?php
+function addPokemon($name, $weight, $type, $date_of_birth, $last_visit, $insurance) 
+{
+  global $db; 
+
+  $query = "insert into pokemon values (:name, :weight, :type, :date_of_birth, :last_visit, :insurance) ";
+  // prepare: 
+  // 1. prepare (compile) 
+  // 2. bindValue + exe
+
+  $statement = $db->prepare($query); 
+  $statement->bindValue(':name', $name);
+  $statement->bindValue(':weight', $weight);
+  $statement->bindValue(':type', $type);
+  $statement->bindValue(':date_of_birth', $date_of_birth);
+  $statement->bindValue(':last_visit', $last_visit);
+  $statement->bindValue(':insurance', $insurance);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function getAllPokemon()
+{
+  global $db;
+  $query = "select * from pokemon";
+  $statement = $db->prepare($query); 
+  $statement->execute();
+  $results = $statement->fetchAll();   // fetch()
+  $statement->closeCursor();
+  return $results;
+}
+  
+// function updatePokemonByName($name, $major, $year)
+// {
+//   global $db;
+//   $query = "update friends set major=:major, year=:year where name=:name";
+
+//   $statement = $db->prepare($query); 
+//   $statement->bindValue(':name', $name);
+//   $statement->bindValue(':major', $major);
+//   $statement->bindValue(':year', $year);
+//   $statement->execute();
+//   $statement->closeCursor();
+// }
+
+function deletePokemonByID($pokemon_ID)
+{
+  global $db;
+  $query = "delete from pokemon where pokemon_ID=:pokemon_ID";
+  $statement = $db->prepare($query); 
+  $statement->bindValue(':pokemon_ID', $pokemon_ID);
+  $statement->execute();
+  $statement->closeCursor();
+
+}
+?>
