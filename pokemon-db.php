2245d12fe7495127ccaebf88b3b44c603a3d92aa
cfkim <christine.fth.kim@gmail.com>
2023-11-02 21:02:19 -0400

    Implement update pokemon function.

--- pokemon-db.php
+++ pokemon-db.php
@@ -30,10 +30,10 @@ function getAllPokemon()
   return $results;
 }
 
-function updatePokemonByID($pokemon_ID, $name, $weight, $type, $insurance)
+function updatePokemonByID($pokemon_ID, $name, $weight, $type, $date_of_birth, $last_visit, $insurance)
 {
   global $db;
-  $query = "update pokemon set name=:name, weight=:weight, type=:type, insurance=:insurance where pokemon_ID=:pokemon_ID";
+  $query = "update pokemon set name=:name, weight=:weight, type=:type, date_of_birth:date_of_birth, last_visit=:last_visit, insurance=:insurance where pokemon_ID=:pokemon_ID";
 
   $statement = $db->prepare($query);
   $statement->bindValue(':name', $name);
