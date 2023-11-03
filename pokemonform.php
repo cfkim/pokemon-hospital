c642730769719ece6aade66478f88d731c9ee062
sarahyl <sarah.liu.va@gmail.com>
2023-11-02 21:13:30 -0400

    add and delete

--- pokemonform.php
+++ pokemonform.php
@@ -2,7 +2,10 @@
 require("connect-db.php");
 require("pokemon-db.php");
 
+
+
 $list_of_pokemon = getAllPokemon();
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
@@ -25,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
    {
     updatePokemonByID($_POST['pokemon_ID'], $_POST['name'], $_POST['weight'], $_POST['type'], $_POST['insurance']);
     $list_of_pokemon = getAllPokemon();
+   }
 }
 
 ?>
@@ -77,10 +81,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         <input type="submit" value="Add Patient" name="addBtn" 
                 class="btn btn-primary" title="Add a patient to the pokemon health center" />
       </div>
-      <div class="row mb-3 mx-3">
-        <input type="submit" value="Confirm Update" name="confirmUpdateBtn"
-                class="btn btn-secondary" title="Update a pokemon into pokemon table" />
-      </div>
     </form>     
 
 <hr/>
@@ -112,15 +112,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
      <td><?php echo $pokemon['date_of_birth']; ?></td> 
      <td><?php echo $pokemon['last_visit']; ?></td> 
      <td><?php echo $pokemon['insurance']; ?></td>
-    <td>
-        <form action="pokemonform.php" method="post">
-          <input type="submit" value="Update" name="updateBtn" class="btn btn-secondary"/></td>
-          <input type="hidden" name="pokemon_ID" value="<?php echo pokemon['pokemon_ID']; ?>"/>
-          <input type="hidden" name="name_to_update" value="<?php echo pokemon['name']; ?>"/>
-          <input type="hidden" name="weight_to_update" value="<?php echo $pokemon['weight']; ?>"/>
-          <input type="hidden" name="type_to_update" value="<?php echo $pokemon['type']; ?>"/>
-          <input type="hidden" name="insurance_to_update" value="<?php echo $pokemon['insurance']; ?>"/>
-        </form>
     <td>
       <form action="pokemonform.php" method="post">
         <input type="submit" value="Delete" name="deleteBtn" class="btn btn-danger"/>
