5087ded4b65eb12effc449cc929bf15e2a70271f
sarahyl <sarah.liu.va@gmail.com>
2023-12-03 16:05:41 -0500

    css

--- myprofile.php
+++ myprofile.php
@@ -22,6 +22,8 @@ $specialities = getNurseSpecialties($_SESSION['user']['nurse_ID'])
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="styles.css">
     </head>
 
